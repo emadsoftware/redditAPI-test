3506644998eed02df4b4c14dfa763beb29b844f8
EA <icyrealm@gmail.com>
2020-12-17 08:02:19 -0500

    Completed version 1 of app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import './App.css';
 import React from 'react';
 import FetchData from './components/FetchData';
+import MetaTags from 'react-meta-tags';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 class App extends React.Component {
   render() {  
     return <div className="App">
+      <MetaTags>
+        <title>Reactive Reading App v.1</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+      </MetaTags>
       <header className="App-header">        
-        <p>Welcome to my react app!</p>        
+        <h1>Reactive Reading App v.1</h1>   
+        <p>✅Set cards amount ✅Set text length ✅No annoying images</p>     
       </header> 
       <FetchData></FetchData> 
                 
--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -7,44 +7,81 @@ export default class FetchData extends React.Component {
     constructor(props){
         super(props);
         this.md = new Remarkable();
-        //this.handleCHange = this.handleCHange.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleChange2 = this.handleChange2.bind(this);
         this.state = {
             loading: true,
             response: [],
-            value: "Text here"
+            cardsToShow: "5",
+            cardTextLength: 600
         };
     }
 
- 
+    handleChange(e) {        
+        this.setState({ loading: true, cardsToShow: e.target.value });
+    } 
+
+    handleChange2(e) {        
+        this.setState({ cardTextLength: e.target.value });
+    } 
 
     async componentDidMount(){
+        this.myFunction();        
+    }
+
+    async componentDidUpdate(){
+        this.myFunction();
+    }
+
+    async myFunction(){
         // get data
         const url = 'https://www.reddit.com/r/webdev.json';
         const response = await fetch(url);
         const data = await response.json();
 
-        // narrow down results
+        // set data
         const dataArr = data.data.children;
         //this.setState({ response: dataArr });
-        this.setState({ response: dataArr.slice(0,15) })
+        this.setState({ response: dataArr.slice(0,this.state.cardsToShow) });
+        this.setState({loading: false});
 
-        // test data
-        console.log(JSON.stringify(this.state.response[0].data.title));
-        console.log(JSON.stringify(this.state.response[2].data));
-        
-        console.log(this.state.response.length);
+        // log data
+        // console.log(JSON.stringify(this.state.response[0].data.title));
+        // console.log(JSON.stringify(this.state.response[2].data));        
+        // console.log(this.state.response.length);
     }
 
     render() {
         return    <div>
-                      {
-                          this.state.response.map(((item, index) => 
+                        <div className="inputGroup">                        
+                            <label>Cards:</label>
+                            <input
+                                type="number"
+                                id="markdown-content"
+                                onChange={this.handleChange}
+                                defaultValue={this.state.cardsToShow}
+                            />  
+                            <label>Length:</label>
+                            <input
+                                type="number"
+                                id="markdown-content"
+                                onChange={this.handleChange2}
+                                defaultValue={this.state.cardTextLength}
+                            />
+                            <label>{ this.state.loading ? 'Loading...' : ''}</label>
+                        </div>                                             
+
+                        {
+                            this.state.response.map(((item, index) => 
                             <div className='card-component'>         
-                                  <div className='title'>{item.data.title}</div>                    
-                                  <div className='message'><ReactMarkdown>{item.data.selftext.substr(0,600)}</ReactMarkdown></div>
-                              </div>
-                          ))
-                      }
+                                    <div className='title'>{item.data.title}</div>                    
+                                    <div className='message'> 
+                                        <ReactMarkdown>{item.data.selftext.substr(0,this.state.cardTextLength)}</ReactMarkdown>
+                                        { this.state.cardTextLength < item.data.selftext.length ? '...' : ''}
+                                    </div>
+                                </div>
+                            ))
+                        }
                   </div>;
     }
 }
